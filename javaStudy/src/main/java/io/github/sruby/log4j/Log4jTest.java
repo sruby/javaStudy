7ce1ded2eaf9c4ef14970058eba58f4e57df2ece
sruby <sruby@192.168.1.101>
2016-08-12 02:28:40 +0800

    log4j

---
+++ javaStudy/src/main/java/io/github/sruby/log4j/Log4jTest.java
@@ -0,0 +1,12 @@
+package io.github.sruby.log4j;
+
+import org.apache.log4j.Logger;
+
+public class Log4jTest
+{
+	public static void main(String[] args)
+	{
+		Logger logger = Logger.getLogger(Log4jTest.class.getName());
+		logger.info(1);
+	}
+}
