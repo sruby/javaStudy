ce4bd4f642f81bc4070048ad1dde0cd4eecbce6d
sruby <hisruby001@gmail.com>
2016-09-13 13:53:49 +0800

    mybatis

--- javaStudy/src/main/java/io/github/sruby/mybatis/test/MybatisTest.java
+++ javaStudy/src/main/java/io/github/sruby/mybatis/test/MybatisTest.java
@@ -4,8 +4,7 @@ import io.github.sruby.mybatis.model.User;
 import io.github.sruby.mybatis.model.UserMapper;
 
 import java.io.Reader;
-
-import junit.framework.TestCase;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -24,6 +23,9 @@ public class MybatisTest
 	
 	private static Reader reader;
 	
+	private SqlSession session = sqlSessionFactory.openSession();
+	
+	UserMapper userOperation = session.getMapper(UserMapper.class);
 	static
 	{
 		try
@@ -50,7 +52,6 @@ public class MybatisTest
 	public void testQueryByNamacespace()
 	{
 		//SqlSession 实例来直接执行已映射的SQL语句
-		SqlSession session = sqlSessionFactory.openSession();
 		try
 		{
 			User user = (User) session.selectOne("io.github.sruby.mybatis.model.UserMapper.selectUserByID", 1);
@@ -70,13 +71,95 @@ public class MybatisTest
 	@Test
 	public void testQueryByInterface()
 	{
-		SqlSession session = sqlSessionFactory.openSession();
+		
 		try
 		{
-			UserMapper userOperation = session.getMapper(UserMapper.class);
+			
 			User user = userOperation.selectUserByID(1);
 			System.out.println(user.getUserAddress());
 			System.out.println(user.getUserName());
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	/**
+	 * 查询用户列表
+	 * @author liuwf on Sep 12, 2016 9:41:26 PM
+	 */
+	@Test
+	public void testSelectUsers()
+	{
+		try
+		{
+			List<User> users = userOperation.selectUsers("%");
+			for (User user : users)
+			{
+				System.out.println(user.getId() + ":" + user.getUserName() + ":" + user.getUserAddress());
+			}
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	/**
+	 * 添加用户
+	 * @author liuwf on Sep 12, 2016 9:48:27 PM
+	 */
+	@Test
+	public void testAddUser()
+	{
+		try
+		{
+			User user = new User();
+			user.setUserAddress("人民广场");
+			user.setUserName("飞鸟");
+			user.setUserAge("80");
+			
+			userOperation.addUser(user);
+			session.commit();
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	/**
+	 * 更新用户信息
+	 * @author liuwf on Sep 12, 2016 9:48:27 PM
+	 */
+	@Test
+	public void testUpdateUser()
+	{
+		try
+		{
+			User user = userOperation.selectUserByID(1);
+			user.setUserAddress("原来是魔都的浦东创新园区");
+			userOperation.updateUser(user);
+			session.commit();
+		}
+		finally
+		{
+			session.close();
+		}
+	}
+	
+	/**
+	 * 删除数据
+	 * @author liuwf on Sep 12, 2016 9:48:27 PM
+	 */
+	@Test
+	public void testDeleteUser()
+	{
+		try
+		{
+			userOperation.deleteUser(1);
+			session.commit();
 		}
 		finally
 		{
