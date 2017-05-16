82306c56d776fdb45866df4d9bb58af357355115
sruby <hiSruby001@gmail.com>
2017-05-16 20:16:14 +0800

    切面和注解实现业务前置

--- src/main/java/io/github/sruby/annotation/point/Client.java
+++ src/main/java/io/github/sruby/annotation/point/Client.java
@@ -1,5 +1,10 @@
 package io.github.sruby.annotation.point;
 
+import io.github.sruby.spring.aop.xml.Student;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * 测试类
  * @author liuwf on May 6, 2017 6:36:59 PM
@@ -8,7 +13,12 @@ public class Client
 {
 	public static void main(String[] args)
 	{
-		LoginService loginService = new LoginService();
+//		LoginService loginService = new LoginService();
+		/**
+		 * 切面的对象必须通过spring来管理
+		 */
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		LoginService loginService = (LoginService) context.getBean("loginService");
 		loginService.login();
 	}
 }
