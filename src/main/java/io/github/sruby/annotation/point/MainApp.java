98a2a5be7cb7ce885e0b81ef4c2102c530fbc948
sruby <hiSruby001@gmail.com>
2017-05-18 22:30:56 +0800

    切面和注解实现业务前置

---
+++ src/main/java/io/github/sruby/annotation/point/MainApp.java
@@ -0,0 +1,22 @@
+package io.github.sruby.annotation.point;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 基于xml的切面示例
+ * 注意：bean.xml配置需要在beans节点使用aop的schema
+ * 需要放开xml中的aop节点注释
+ * @author sruby on 2016年10月10日 上午12:14:29
+ */
+public class MainApp
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		Student student = (Student) context.getBean("student2");
+		student.getName();
+		student.getAge();
+//		student.printThrowException();
+	}
+}
